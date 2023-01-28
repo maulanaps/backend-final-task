d6a77aa338601f91e794876cec1dcf75f2bb21dd
maulanaps <maulanaputra.official@gmail.com>
2023-01-28 19:31:58 +0700

    re-code getReport service

--- src/main/java/com/backend/task/repo/UserRepo.java
+++ src/main/java/com/backend/task/repo/UserRepo.java
@@ -1,10 +1,15 @@
 package com.backend.task.repo;
 
 import com.backend.task.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface UserRepo extends CrudRepository<User, Integer> {
     User findByUsername(String username);
     User findByKtp(String ktp);
     User getById(Integer id);
+    @Query("SELECT username FROM User")
+    List<String> findAllUsername();
 }
--- src/main/java/com/backend/task/services/ReportServices.java
+++ src/main/java/com/backend/task/services/ReportServices.java
@@ -2,15 +2,16 @@ package com.backend.task.services;
 
 import com.backend.task.dto.ReportDto;
 import com.backend.task.models.Transaction;
-import com.backend.task.models.User;
 import com.backend.task.repo.TransactionRepo;
 import com.backend.task.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ReportServices {
@@ -19,68 +20,118 @@ public class ReportServices {
     @Autowired
     UserRepo userRepo;
 
-    public List<ReportDto> getReports(LocalDate localDate){
+    public List<ReportDto> getReports(LocalDate localDate) {
 
-        // initialize response dto
-        List<ReportDto> reports = new ArrayList<>();
+        // get all user's username
+        List<String> usersUsername = userRepo.findAllUsername();
 
-        // get transactions by date
+        // get all trx on certain dat
         List<Transaction> transactions = transactionRepo.findAllByDate(localDate);
 
-        // get all user id
-        Set<Integer> userIds = new HashSet<>();
-        for (var transaction : transactions) {
-            userIds.add(transaction.getUser().getId());
-        }
+        // formatted date
+        String balanceChangeDate = localDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
 
-        // get all transaction of each user
-        Integer latestBalance;
-        Integer oldestBalance;
-        List<Transaction> userTransaction = new ArrayList<>();
-        for (var userId : userIds){
+        // Map users transactions into hashmap
+        var map = new HashMap<String, List<Transaction>>();
+        usersUsername.forEach((username -> {
+            List<Transaction> userTrx = transactions.stream().filter(transaction ->
+                    username.equals(transaction.getUsername())).collect(Collectors.toList());
+            map.put(username, userTrx);
+        }));
 
-            // clear user transaction list
-            userTransaction.clear();
+        // calculate change in balance per user
+        var report = new ArrayList<ReportDto>();
+        map.forEach((username, transactionList) -> {
 
-            // add transaction of the user
-            for (var transaction : transactions) {
-                if (transaction.getUser().getId() == userId){
-                    userTransaction.add(transaction);
+            String changeInPercentage = "";
+
+            // if the user didn't do any trx on that date
+            if (transactionList.size() == 0) {
+                changeInPercentage = "0%";
+            } // calculation
+            else {
+                Integer oldestBalance = transactionList.get(0).getBalanceBefore();
+                Integer currentBalance = transactionList.get(transactionList.size() - 1).getBalanceAfter();
+
+                int numerator = currentBalance - oldestBalance;
+                double denominator = oldestBalance * 1.0;
+
+                // if yesterday balance is Rp0,-
+                if (denominator == 0) {
+                    changeInPercentage = "-";
+                } else {
+                    DecimalFormat df = new DecimalFormat("###.##");
+                    double result = (numerator / denominator) * 100;
+                    changeInPercentage = df.format(result) + "%";
                 }
             }
 
-            // sort by trxId (oldest to latest trx)
-            Collections.sort(userTransaction, new SortbyTrxId());
+            // make ReportDto object per user
+            // add it to report list
+            report.add(new ReportDto(username, changeInPercentage, balanceChangeDate));
+        });
 
-            // response data
-            oldestBalance = userTransaction.get(0).getBalanceBefore();
-            latestBalance = userTransaction.get(userTransaction.size() - 1).getBalanceAfter();
+        return report;
 
-            User user = userRepo.getById(userId);
-            String username = user.getUsername();
-
-            // calculation
-            Integer numerator = latestBalance - oldestBalance;
-            Float denominator = oldestBalance.floatValue();
-            Float temp = numerator / denominator * 100;
-            String changeInPercentage;
-            String balanceChangeDate = localDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
-
-            if (denominator <= 0){
-                changeInPercentage = "-";
-            } else {
-                changeInPercentage = temp + "%";
-            }
-
-            // add into reportDto list
-            reports.add(new ReportDto(username, changeInPercentage, balanceChangeDate));
-        }
-
-        return reports;
+//        // initialize response dto
+//        List<ReportDto> reports = new ArrayList<>();
+//
+//        // get transactions by date
+//        List<Transaction> transactions = transactionRepo.findAllByDate(localDate);
+//
+//        // get all user id
+//        Set<Integer> userIds = new HashSet<>();
+//        for (var transaction : transactions) {
+//            userIds.add(transaction.getUser().getId());
+//        }
+//
+//        // get all transaction of each user
+//        Integer latestBalance;
+//        Integer oldestBalance;
+//        List<Transaction> userTransaction = new ArrayList<>();
+//        for (var userId : userIds){
+//
+//            // clear user transaction list
+//            userTransaction.clear();
+//
+//            // add transaction of the user
+//            for (var transaction : transactions) {
+//                if (transaction.getUser().getId() == userId){
+//                    userTransaction.add(transaction);
+//                }
+//            }
+//
+//            // sort by trxId (oldest to latest trx)
+//            Collections.sort(userTransaction, new SortbyTrxId());
+//
+//            // response data
+//            oldestBalance = userTransaction.get(0).getBalanceBefore();
+//            latestBalance = userTransaction.get(userTransaction.size() - 1).getBalanceAfter();
+//
+//            User user = userRepo.getById(userId);
+//            String username = user.getUsername();
+//
+//            // calculation
+//            Integer numerator = latestBalance - oldestBalance;
+//            Float denominator = oldestBalance.floatValue();
+//            Float temp = numerator / denominator * 100;
+//            String changeInPercentage;
+//            String balanceChangeDate = localDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+//
+//            if (denominator <= 0){
+//                changeInPercentage = "-";
+//            } else {
+//                changeInPercentage = temp + "%";
+//            }
+//
+//            // add into reportDto list
+//            reports.add(new ReportDto(username, changeInPercentage, balanceChangeDate));
+//        }
+//
+//        return reports;
     }
 
-    class SortbyTrxId implements Comparator<Transaction>
-    {
+    class SortbyTrxId implements Comparator<Transaction> {
         // Used for sorting in ascending order of
         @Override
         public int compare(Transaction a, Transaction b) {
